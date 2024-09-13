f5fddb0c8307899acb8320acaa49288d5acc9322
JEduardo <jesseard94@gmail.com>
2024-09-13 11:44:55 -0500

    feat(age_validation): age validation functionality is added

---
+++ AgeValidation.java
@@ -0,0 +1,13 @@
+public class AgeValidation {
+    public static void main(String[] args) {
+        int age = 18;
+        
+        if (age >= 18) {
+            System.out.println("Access granted");
+        }
+        
+        if (age < 18) {
+            System.out.println("Access denied");
+        }
+    }
+}
